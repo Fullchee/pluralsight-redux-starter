1e6457445aff9fe1c9c20735e31475a43780a265
Fullchee Zhang <fullchee@outlook.com>
2018-12-24 21:10:11 -0500

    Fix typo"

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -10,7 +10,7 @@ export function loadAuthors() {
     return authorApi.getAllAuthors().then((authors) => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch((error) => {
-      throw(error)
-    });[]
-  }
+      throw(error);
+    });
+  };
 }
